3bfdfca61c5b35b2034305241ccb9da98fdab34c
exp-table <exp.table@protonmail.com>
2024-11-15 12:33:41 +0700

    test and script directories

---
+++ script/Base.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {Script} from "forge-std/Script.sol";
+
+contract BaseScript is Script {
+    function setUp() public virtual {}
+
+    function run() public virtual {}
+}
